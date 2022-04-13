afe6e963c0ceda95c2377c6edcad261968d320d6
TerryRoberts1995 <terryjbroberts@yahoo.com>
2022-04-12 21:49:57 -0500

    fixed active style bug associated with the auth's login component

--- src/components/pages/auth/login.js
+++ src/components/pages/auth/login.js
@@ -1,68 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import { navigate } from 'hookrouter';
+import React, {useState, useEffect} from 'react';
+import {useAppContext} from '../../Context';
+import {navigate} from 'hookrouter';
 import Cookies from 'js-cookie';
 
-export default function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
+export default function Login () {
+  const [active, setActive] = useAppContext ();
+  const [username, setUsername] = useState ('');
+  const [password, setPassword] = useState ('');
+  const [error, setError] = useState (false);
+  const [errorMessage, setErrorMessage] = useState ('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = e => {
+    e.preventDefault ();
 
-        if(username === '' || password === '') {
-            setError(true);
-            setErrorMessage('Error: All fields must be completed.');
-        } else {
-            fetch('https://hookstore-api.herokuapp.com/user/verify', {
-                method: 'POST',
-                headers: { 'content-type': 'application/json'},
-                body: JSON.stringify({
-                    username: username,
-                    password: password
-                })
-            })
-            .then(res => res.json())
-            .then(res => {
-                if(res === 'User NOT verified') {
-                    setError(true);
-                    setErrorMessage('Error: User NOT verified');
-                } else if(res === "User has been verified"){
-                    setError(false);
-                    setErrorMessage('');
-                    Cookies.set('username', username);
-                    navigate('/');
-                }
-            })
-            .catch(error => {
-                console.log('Error with logging in, please try again.', error);
-                setError(true);
-                setErrorMessage('Error with logging in, please try again.');
-            })
-        }
+    if (username === '' || password === '') {
+      setError (true);
+      setErrorMessage ('Error: All fields must be completed.');
+    } else {
+      fetch ('https://hookstore-api.herokuapp.com/user/verify', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify ({
+          username: username,
+          password: password,
+        }),
+      })
+        .then (res => res.json ())
+        .then (res => {
+          if (res === 'User NOT verified') {
+            setError (true);
+            setErrorMessage ('Error: User NOT verified');
+          } else if (res === 'User has been verified') {
+            setError (false);
+            setErrorMessage ('');
+            Cookies.set ('username', username);
+            setActive ('home');
+            navigate ('/');
+          }
+        })
+        .catch (error => {
+          console.log ('Error with logging in, please try again.', error);
+          setError (true);
+          setErrorMessage ('Error with logging in, please try again.');
+        });
     }
+  };
 
-    useEffect(() => {
-        setError(false);
-        setErrorMessage("")
-    },[username, password])
+  useEffect (
+    () => {
+      setError (false);
+      setErrorMessage ('');
+    },
+    [username, password]
+  );
 
-    return (
-        <div className='login-container main-body'>
-            <form className="login-form" onSubmit={(e) => handleSubmit(e)}>
-                <h2>Login in here</h2>
-                
-                <div className="login-form-input">
-                    <input type='text' placeholder='Username' value={username} name='username' onChange={(e) => setUsername(e.target.value)} />
-                    <input type='text' placeholder='Password' value={password} name='password' onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                
+  return (
+    <div className="login-container main-body">
+      <form className="login-form" onSubmit={e => handleSubmit (e)}>
+        <h2>Login in here</h2>
 
-                <button className="btn" type='submit'>Submit</button>
-            </form>
-
-            <h3 style={{visibility: error ? 'visible' : 'hidden'}}>{errorMessage}</h3>
+        <div className="login-form-input">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            name="username"
+            onChange={e => setUsername (e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            value={password}
+            name="password"
+            onChange={e => setPassword (e.target.value)}
+          />
         </div>
-    )
+
+        <button className="btn" type="submit">Submit</button>
+      </form>
+
+      <h3 style={{visibility: error ? 'visible' : 'hidden'}}>{errorMessage}</h3>
+    </div>
+  );
 }
